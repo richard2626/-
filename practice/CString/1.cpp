19089220ac2d440b79b490d4fe8df3eedede283a
richard2626 <li.chichih>
2023-01-01 11:33:09 +0800

    update

---
+++ practice/CString/1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cstring>
+
+int main()
+{
+    char s[100] = {"0123456789"};
+    char c;
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%s\n", s);
+        c = s[0];
+        strcpy(s, &s[1]);
+        strcat(s, &c);
+    }
+}
